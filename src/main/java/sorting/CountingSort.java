fa03d22e37634259111132ac79970ab03b583872
João Brunet <joaoarthurbm@gmail.com>
2024-03-01 10:29:34 -0300

    Update CountingSort.java

--- src/main/java/sorting/CountingSort.java
+++ src/main/java/sorting/CountingSort.java
@@ -7,27 +7,7 @@ public class CountingSort {
     * criar métodos auxiliares se precisar.
     */
     public int[] classicCountingSort(int[] a, int k) {
-         int[] c = new int[k];
-
-        // frequência
-        for (int i = 0; i < a.length; i++) {
-            c[a[i] - 1] += 1;
-        }
-        
-        // cumulativa
-        for (int i = 1; i < c.length; i++) {
-            c[i] += c[i-1];
-        }
-
-        int[] b = new int[a.length];
-
-        for (int i = a.length - 1; i >= 0; i--) {
-            b[c[a[i] - 1] - 1] = a[i];
-            c[a[i] - 1] -= 1;
-        }
-
-        return b;
-    
+       return null;
     }
 
     /**
