ce0d8ac0dc33e1c1bd33a6e322542307bd6cf6cf
sabrinasousaq <sabrinaoliveiraoliveira10@gmail.com>
2026-05-13 16:06:02 -0300

    .

--- src/main/java/CountingSort.java
+++ src/main/java/CountingSort.java
@@ -53,8 +53,37 @@ public class CountingSort {
     * vai precisar identificar o menor elemento do array. Faça isso no início do método.
     */
     public int[] negativosCountingSort(int[] v, int k) {
-        // TODO implementar
-        return null;
-    }
+     int menor = v[0];
+     int maior = v[0];
 
+	for(int i = 1; i < v.length; i++) {
+
+		if(v[i] < menor) {
+			menor = v[i];
+               }
+
+		if(v[i] > maior) {
+			maior = v[i];
+               }
+	}
+
+       int[] c = new int[maior - menor + 1];
+       //frequência
+       for(int i = 0; i < v.length; i++) {
+            c[v[i] - menor] += 1;
+       }
+       //cumulativa
+       for(int i = 1; i < c.length; i++) {
+            c[i] += c[i-1];
+       }
+       int[] b = new int[v.length];
+       //ordenada
+       for(int i = v.length-1; i >= 0; i--) {
+            b[c[v[i] - menor] - 1] = v[i];
+            c[v[i] - menor] --;
+       }
+
+       return b;
+    }
+    }
 }
