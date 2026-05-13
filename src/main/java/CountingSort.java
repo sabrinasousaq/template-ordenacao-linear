f18e51385849a2e7dc68fd2bd27732f915844dcf
sabrinasousaq <sabrinaoliveiraoliveira10@gmail.com>
2026-05-13 09:29:10 -0300

    zeroCounting em processo

--- src/main/java/CountingSort.java
+++ src/main/java/CountingSort.java
@@ -5,24 +5,69 @@ public class CountingSort {
     * criar métodos auxiliares se precisar.
     */
     public int[] classicCountingSort(int[] a, int k) {
-       return null;
+
+       int[] c = new int[k];
+       //frequência
+       for(int i = 0; i < a.length; i++) {
+            c[a[i]-1] += 1;
+       }
+       //cumulativa
+       for(int i = 1; i < c.length; i++) {
+            c[i] += c[i-1];
+       }
+       int[] b = new int[a.length];
+       //ordenada
+       for(int i = a.length-1; i >= 0; i--) {
+            b[c[a[i]-1] - 1] = a[i];
+            c[a[i]-1] --;
+       }
+
+       return b;
     }
 
     /**
     * Implemente uma versão do counting sort que aceita valor 0 na coleção original.
     */
     public int[] zeroCountingSort(int[] v, int k) {
-        // TODO implementar
-        return null;
+
+        int menor = 0;
+        int maior = 0;
+        for(int i = 0; i < v.length - 1; i++) {
+            if(v[i] > v[i + 1]) {
+                menor = v[i + 1];
+                maior = v[i];
+            } else {
+                menor = v[i];
+                maior = v[i + 1];
+            }
+        }
+            
+        int[] c = new int[maior - menor + 1];
+        //frequência
+        for(int i = 0; i < v.length; i++) {
+                c[v[i] - menor] += 1;
+        }
+        //cumulativa
+        for(int i = 1; i < c.length; i++) {
+                c[i] += c[i-1];
+        }
+        int[] b = new int[v.length];
+        //ordenada
+        for(int i = v.length-1; i >= 0; i--) {
+                b[c[v[i] - menor] - 1] = v[i];
+                c[v[i] - menor] --;
+        }
+
+        return b;
     }
 
     /**
     * Implemente uma versão do counting sort que aceita valores negativos na coleção original. Você
     * vai precisar identificar o menor elemento do array. Faça isso no início do método.
     */
-    public int[] negativosCountingSort(int[] v, int k) {
-        // TODO implementar
-        return null;
-    }
+    // public int[] negativosCountingSort(int[] v, int k) {
+    //     // TODO implementar
+    //     return null;
+    // }
 
 }
